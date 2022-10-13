4d52c95503ed996733b474ea296557d54d78f5c4
xulihang <xulihanghai@163.com>
2022-10-13 17:18:21 +0800

    add capacitor android to the example

---
+++ example/android/app/src/main/java/com/dynamsoft/textscanner/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dynamsoft.textscanner;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ example/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dynamsoft.textscanner',
+  appName: 'text-scanner',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
