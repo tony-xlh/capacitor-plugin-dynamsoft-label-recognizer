20a58c5d4eda32257ed6c942967240f6292121a5
xulihang <xulihanghai@163.com>
2022-10-13 13:36:50 +0800

    add web implementation

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,8 @@
+import { DLRResult } from "dynamsoft-label-recognizer";
+
 export interface LabelRecognizerPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  init(): Promise<void>;
+  initLicense(options: { license: string }): Promise<void>;
+  recognizeBase64String(options: { base64: string }): Promise<DLRResult[]>;
+  setEngineResourcesPath(options: { path: string }): Promise<void>;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,24 @@
 import { WebPlugin } from '@capacitor/core';
+import { DLRResult, LabelRecognizer } from 'dynamsoft-label-recognizer';
 
 import type { LabelRecognizerPlugin } from './definitions';
 
 export class LabelRecognizerWeb extends WebPlugin implements LabelRecognizerPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  private recognizer: LabelRecognizer | null = null;
+  async setEngineResourcesPath(options: { path: string; }): Promise<void> {
+    LabelRecognizer.engineResourcePath = options.path;
+  }
+  async init(): Promise<void> {
+    this.recognizer = await LabelRecognizer.createInstance();
+  }
+  async initLicense(options: { license: string; }): Promise<void> {
+    LabelRecognizer.license = options.license;
+  }
+  async recognizeBase64String(options: { base64: string; }): Promise<DLRResult[]> {
+    if (this.recognizer) {
+      return await this.recognizer.recognizeBase64String(options.base64);
+    }else{
+      throw new Error("Not initialized");
+    }
   }
 }
