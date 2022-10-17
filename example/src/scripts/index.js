b4ac6abdfaa63243940662cadd82d1830cbc7290
xulihang <xulihanghai@163.com>
2022-10-17 10:57:27 +0800

    use different licenses for different platforms

--- example/src/scripts/index.js
+++ example/src/scripts/index.js
@@ -10,9 +10,14 @@ document.getElementsByClassName("capture-button")[0].addEventListener("click", c
 document.getElementsByClassName("use-case")[0].addEventListener("change", changeUseCase);
 
 window.onload = async function(){
-  let license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAwMjI3NzYzLVRYbFhaV0pRY205cVgyUnNjZyIsIm9yZ2FuaXphdGlvbklEIjoiMTAwMjI3NzYzIiwiY2hlY2tDb2RlIjotNTI2ODU2NjYxfQ==";
+  let privateTrial;
+  if (Capacitor.isNativePlatform()) {
+    privateTrial = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAwMjI3NzYzLVRYbE5iMkpwYkdWUWNtOXFYMlJzY2ciLCJvcmdhbml6YXRpb25JRCI6IjEwMDIyNzc2MyIsImNoZWNrQ29kZSI6LTE4MDg2NTM3MjV9";
+  }else{
+    privateTrial = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAwMjI3NzYzLVRYbFhaV0pRY205cVgyUnNjZyIsIm9yZ2FuaXphdGlvbklEIjoiMTAwMjI3NzYzIiwiY2hlY2tDb2RlIjotNTI2ODU2NjYxfQ==";
+  }
   //let publicTrial = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
-  await LabelRecognizer.initLicense({license:license});
+  await LabelRecognizer.initLicense({license:privateTrial});
   await LabelRecognizer.init();
   if (Capacitor.isNativePlatform() === false) {
     LabelRecognizer.addListener('onResourcesLoadStarted', () => {
