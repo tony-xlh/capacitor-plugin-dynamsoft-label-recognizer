2aad5bf6623d7cf2d7f49fabf4024dd7222a4aba
xulihang <xulihanghai@163.com>
2022-10-16 23:14:30 -0700

    complete iOS implementation

--- ios/Plugin/LabelRecognizer.swift
+++ ios/Plugin/LabelRecognizer.swift
@@ -1,33 +1,65 @@
 import Foundation
+import DynamsoftCore
+import DynamsoftLabelRecognizer
 
-@objc public class LabelRecognizer: NSObject {
+@objc public class LabelRecognizer: NSObject, LicenseVerificationListener {
+    private var recognizer:DynamsoftLabelRecognizer!;
+    
     @objc public func echo(_ value: String) -> String {
         print(value)
         return value
     }
     
-    @objc public func initialize(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func initialize() {
+        recognizer = DynamsoftLabelRecognizer.init()
     }
     
-    @objc public func initLicense(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func initLicense(_ license: String) {
+        DynamsoftLicenseManager.initLicense(license,verificationDelegate:self)
     }
     
-    @objc public func recognizeBase64String(_ value: String) -> String {
-        print(value)
-        return value
+    public func licenseVerificationCallback(_ isSuccess: Bool, error: Error?) {
+        
     }
     
-    @objc public func updateRuntimeSettings(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func recognizeBase64String(_ base64: String) -> [Any] {
+        var returned_results: [Any] = []
+        let image = Utils.convertBase64ToImage(base64)
+        if image != nil {
+            let results = try? recognizer.recognizeImage(image!)
+            for result in results! {
+                returned_results.append(Utils.wrapDLRResult(result:result))
+            }
+        }
+        return returned_results
     }
     
-    @objc public func resetRuntimeSettings(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func updateRuntimeSettings(_ template: String) {
+        try? recognizer.initRuntimeSettings(template)
+    }
+    
+    public func loadCustomModel(modelFolder:String,modelFileNames: [String]){
+        for model in modelFileNames {
+            guard let prototxt = Bundle.main.url(
+                forResource: model,
+                withExtension: "prototxt",
+                subdirectory: modelFolder
+            ) else {
+                print("model not exist")
+                return
+            }
+
+            let datapro = try! Data.init(contentsOf: prototxt)
+            let txt = Bundle.main.url(forResource: model, withExtension: "txt", subdirectory: modelFolder)
+            let datatxt = try! Data.init(contentsOf: txt!)
+            let caffemodel = Bundle.main.url(forResource: model, withExtension: "caffemodel", subdirectory: modelFolder)
+            let datacaf = try! Data.init(contentsOf: caffemodel!)
+            DynamsoftLabelRecognizer.appendCharacterModel(model, prototxtBuffer: datapro, txtBuffer: datatxt, characterModelBuffer: datacaf)
+            print("load model %@", model)
+        }
+    }
+    
+    @objc public func resetRuntimeSettings() {
+        try? recognizer.resetRuntimeSettings()
     }
 }
--- ios/Plugin/LabelRecognizerPlugin.swift
+++ ios/Plugin/LabelRecognizerPlugin.swift
@@ -6,7 +6,7 @@ import Capacitor
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(LabelRecognizerPlugin)
-public class LabelRecognizerPlugin: CAPPlugin {
+public class LabelRecognizerPlugin: CAPPlugin  {
     private let implementation = LabelRecognizer()
 
     @objc func echo(_ call: CAPPluginCall) {
@@ -17,37 +17,37 @@ public class LabelRecognizerPlugin: CAPPlugin {
     }
     
     @objc func initialize(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+        implementation.initialize()
+        call.resolve()
     }
     
     @objc func initLicense(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+        let license = call.getString("license") ?? "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ=="
+        implementation.initLicense(license)
+        call.resolve()
     }
     
     @objc func recognizeBase64String(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+        let base64 = call.getString("base64") ?? ""
+        call.resolve(["results":implementation.recognizeBase64String(base64)])
     }
     
     @objc func updateRuntimeSettings(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+        let template = call.getString("template") ?? ""
+        implementation.updateRuntimeSettings(template)
+        
+        guard let customModelConfig = call.getAny("customModelConfig") as? [String:Any] else {
+            call.resolve()
+            return
+        }
+        let modelFolder = customModelConfig["customModelFolder"] as! String
+        let modelFileNames = customModelConfig["customModelFileNames"] as! [String]
+        implementation.loadCustomModel(modelFolder: modelFolder, modelFileNames: modelFileNames)
+        call.resolve()
     }
     
     @objc func resetRuntimeSettings(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+        implementation.resetRuntimeSettings()
+        call.resolve()
     }
 }
